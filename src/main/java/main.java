c3437b9eadc987700b88db241c25848b6e79ccae
Cameron Connor <94772@farnborough.ac.uk>
2020-11-20 10:57:40 +0000

    Completed Hex-Decimal

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,31 @@
+import java.util.Dictionary;
+import java.util.Scanner;
 
 public class main {
 
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println(doBinaryToDecimal("00101010"));
-        System.out.print(doDecimalToBinary(255));
+        /*System.out.println(doBinaryToDecimal("00101010"));
+        System.out.print(doDecimalToBinary(255));*/
+
+        doGreetMenu();
 
     }
+
+    private  static void doGreetMenu(){
+        System.out.println("Enter 0 to convert Binary to Decimal,\n1 To Convert Decimal to Binary,\n2 To Convert Decimal to Hex\n3 to Convert Hex to Decimal.");
+        switch (scanner.nextLine()){
+            case "0":
+                System.out.println(doBinaryToDecimal("101001"));
+                break;
+            case "1":
+                System.out.println(doDecimalToBinary(15));
+                break;
+            case "3":
+                doHexToBin("34FA");
+        }
+    }
+
     public static int doBinaryToDecimal(String input){
         int runningTotal=0; //!Todo make better bc this is slow
         for (int i = 0; i< input.length();i++){
@@ -27,6 +47,25 @@ public class main {
             }else{runningString+=("0");}
         }
 
-        return  runningString;
+        return  Integer.toString(Integer.parseInt(runningString)); //Removes leading zeros
     }
+
+    public static int doHexToBin(String input){
+        input = input.toUpperCase();
+        String newInput = "";
+        for(int i = 0; i<input.length();i++){
+            newInput += input.charAt(input.length()-(i+1));
+        }
+        int runningTotal = 0;
+        String runningString = "";
+        for(int i = 0;i<newInput.length();i++){
+            try{
+                runningTotal += (Character.getNumericValue(newInput.charAt(i))*(Math.pow(16,i)));
+            }catch (Exception e){
+                return -1;
+            }//!TODO ASCII VAL to number
+        }
+        return runningTotal;
+    }
+
 }
